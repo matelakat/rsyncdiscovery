f3d1980ad54913a93e2416d92e009fec55063e6f
Mate Lakat <mlakat@mlakat.hu>
2013-03-16 12:11:58 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 rsyncdiscovery
 ==============
 
-A script to find out rsync parameters
+A test script to find out rsync parameters.
+
+I would like to create a file-level backup of some files. My goal is to use
+rsync to do the job. As rsync has a lot of command line switches, first, I
+would need to know, which ones to use. For this, I have some expectations:
+  1. Backup is recursive
+  2. Extra files removed
+  3. Links are preserved
+  4. Symlinks are preserved
+  5. Symlinks are not rewritten
+  6. Preserve permissions
+  7. Modification times are preserved
+  8. Special files reserved
+
+And of course, a test-driven approach will be used. Bash does not prevent you
+from writing tests, so go ahead.
+
+Ending slash: The ending slash is important on the source side, if you wish to
+say to copy the contents of that directory, not the directory itself.
+
+The archive mode includes:
+
+ - `r` - recursive
+ - `l` - copy symlinks as symlinks
+ - `p` - preserve permissions
+ - `t` - preserve modification times
+ - `g` - preserve group
+ - `o` - preserve owner
+ - `D` - preserve device and special files
+
+And the manual also states, that it does not include:
+ - `H` - preserve hard links
+ - `A` - preserve ACLs
+ - `X` - preserve extended attributes
+
+For me the hardlinks seem to be important.
---
+++ test_rsync.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+function backup {
+    rsync -r -H -l -g -o -t -D -p --del $1 $2
+}
+
+function test_backup_is_recursive {
+    touch src/f1
+    backup src tgt
+    [ -e tgt/src/f1 ]
+}
+
+function test_extra_files_removed {
+    mkdir tgt/src
+    touch tgt/src/f1
+    backup src tgt
+    [ ! -e tgt/src/f1 ]
+}
+
+function test_hardlinks_preserved {
+    echo "k" >  src/f1
+    link src/f1 src/f2
+    backup src tgt
+    [ -e tgt/src/f2 ]
+    diff <( cd src; du ) <( cd tgt/src; du )
+}
+
+function test_symlinks_preserved {
+    touch src/symlinktarget
+    ln -s src/symlinktarget src/symlink
+    [ -L src/symlink ] # Sanity check
+    backup src tgt
+    [ -L tgt/src/symlink ]
+}
+
+function test_symlinks_not_rewritten {
+    touch src/symlinktarget
+    ln -s src/symlinktarget src/symlink
+    backup src tgt
+
+    diff <( readlink src/symlink ) <( readlink tgt/src/symlink )
+}
+
+function test_group_preserved {
+    touch src/f1
+    chown 1001:1003 src/f1
+    backup src tgt
+
+    backup src tgt
+    diff <(stat -c "%g" src/f1) <(stat -c "%g" tgt/src/f1)
+}
+
+function test_owner_preserved {
+    touch src/f1
+    chown 1001:1003 src/f1
+    backup src tgt
+
+    backup src tgt
+    diff <(stat -c "%u" src/f1) <(stat -c "%u" tgt/src/f1)
+}
+
+function test_modification_time_preserved {
+    touch src/f1
+    sleep 1
+    backup src tgt
+
+    backup src tgt
+    diff <(stat -c "%Y" src/f1) <(stat -c "%Y" tgt/src/f1)
+}
+
+function test_special_files_preserved {
+    mkfifo src/f1
+    backup src tgt
+
+    [ -p tgt/src/f1 ]
+}
+
+function test_permissions {
+    touch src/f1
+    chmod 0777 src/f1
+    backup src tgt
+
+    diff <(stat -c "%a" src/f1) <(stat -c "%a" tgt/src/f1)
+}
+
+function run_test {
+    TMPFILE=$(mktemp)
+    TMPDIR=$(mktemp -d)
+    echo -n "$1"
+    [ ! -z "$TMPDIR" ] || return 1
+    (
+        set -exu
+        cd $TMPDIR
+        mkdir src
+        mkdir tgt
+        $1
+    ) > $TMPFILE 2>&1
+    RESULT="$?"
+
+    if [ "0" != "$RESULT" ]
+    then
+        echo " FAILED"
+        cat $TMPFILE
+        find "$TMPDIR"
+    else
+        echo " PASS"
+    fi
+
+    rm -rf $TMPDIR
+    rm -f $TMPFILE
+    return $RESULT
+}
+
+if [ "$1" = "list_tests" ]
+then
+    grep -e "^function *test_" $0 | while read line
+    do 
+        echo "$line" | cut -d" " -f2
+    done
+    exit 0
+else
+    $0 list_tests | while read testcase
+    do
+        run_test $testcase
+    done
+fi
